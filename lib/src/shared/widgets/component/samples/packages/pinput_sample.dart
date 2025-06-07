cb2db0e512127de4018335de7b7789f215f092cb
dariomatias-dev <matiasdario75@gmail.com>
2025-06-07 17:52:02 -0300

    feat(lib/src/shared/widgets/component/samples/packages/pinput_sample.dart): add pinput package example
    - Developed example using the pinput package.

--- lib/src/shared/widgets/component/samples/packages/pinput_sample.dart
+++ lib/src/shared/widgets/component/samples/packages/pinput_sample.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:pinput/pinput.dart';
 
-class PinputSample extends StatelessWidget {
+class PinputSample extends StatefulWidget {
   const PinputSample({super.key});
 
   @override
+  State<PinputSample> createState() => _PinputSampleState();
+}
+
+class _PinputSampleState extends State<PinputSample> {
+  final _pinController = TextEditingController();
+
+  void _onCompleted(String pin) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          'PIN: $pin',
+        ),
+        action: SnackBarAction(
+          label: 'Ok',
+          onPressed: () {
+            _pinController.text = '';
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Pinput(
+          controller: _pinController,
+          length: 4,
+          onCompleted: _onCompleted,
+        ),
+      ),
+    );
   }
 }
