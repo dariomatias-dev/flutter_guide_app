a3804cf271616920c06a537db99800a02dbba34e
dariomatias-dev <matiasdario75@gmail.com>
2025-06-21 11:24:16 -0300

    feat(lib/src/shared/widgets/component/samples/packages/google_mobile_ads_sample.dart): added separation of ad types
    - Separated ad types to improve user experience.

--- lib/src/shared/widgets/component/samples/packages/google_mobile_ads_sample.dart
+++ lib/src/shared/widgets/component/samples/packages/google_mobile_ads_sample.dart
@@ -11,49 +11,48 @@ class GoogleMobileAdsSample extends StatefulWidget {
 
 class _GoogleMobileAdsSampleState extends State<GoogleMobileAdsSample> {
   BannerAd? _bannerAd;
-  InterstitialAd? _interstitialAd;
   bool _isBannerLoaded = false;
 
-  void _loadBannerAd() {
+  InterstitialAd? _interstitialAd;
+  bool _isInterstitialLoaded = false;
+
+  void _loadBanner() {
     _bannerAd = BannerAd(
       adUnitId: dotenv.env['BANNER_AD_SAMPLE_ID']!,
       request: const AdRequest(),
       size: AdSize.banner,
       listener: BannerAdListener(
-        onAdLoaded: (ad) {
-          setState(() {
-            _isBannerLoaded = true;
-          });
+        onAdLoaded: (_) {
+          setState(() => _isBannerLoaded = true);
         },
-        onAdFailedToLoad: (ad, err) {
-          debugPrint('BannerAd failed to load: $err');
+        onAdFailedToLoad: (ad, error) {
+          debugPrint('BannerAd failed to load: $error');
           ad.dispose();
         },
       ),
-    );
-    _bannerAd?.load();
+    )..load();
   }
 
-  Future<void> _loadInterstitialAd() async {
+  void _loadInterstitial() {
     InterstitialAd.load(
       adUnitId: dotenv.env['INTERSTICIAL_AD_SAMPLE_ID']!,
       request: const AdRequest(),
       adLoadCallback: InterstitialAdLoadCallback(
         onAdLoaded: (ad) {
-          setState(() {
-            _interstitialAd = ad;
-          });
+          _interstitialAd = ad;
+          _isInterstitialLoaded = true;
         },
         onAdFailedToLoad: (error) {
           debugPrint('InterstitialAd failed to load: $error');
+          _isInterstitialLoaded = false;
         },
       ),
     );
   }
 
-  Future<void> _showInterstitialAd() async {
-    if (_interstitialAd == null) {
-      debugPrint('InterstitialAd is not loaded yet');
+  void _showInterstitial() {
+    if (!_isInterstitialLoaded || _interstitialAd == null) {
+      debugPrint('InterstitialAd is not ready');
       return;
     }
 
@@ -61,24 +60,28 @@ class _GoogleMobileAdsSampleState extends State<GoogleMobileAdsSample> {
       onAdDismissedFullScreenContent: (ad) {
         ad.dispose();
         _interstitialAd = null;
-        _loadInterstitialAd();
+        _isInterstitialLoaded = false;
+        _loadInterstitial();
       },
       onAdFailedToShowFullScreenContent: (ad, error) {
         ad.dispose();
         _interstitialAd = null;
+        _isInterstitialLoaded = false;
         debugPrint('InterstitialAd failed to show: $error');
-        _loadInterstitialAd();
+        _loadInterstitial();
       },
     );
 
     _interstitialAd!.show();
     _interstitialAd = null;
+    _isInterstitialLoaded = false;
   }
 
   @override
   void initState() {
-    _loadBannerAd();
-    _loadInterstitialAd();
+    _loadBanner();
+    _loadInterstitial();
+
     super.initState();
   }
 
@@ -86,6 +89,7 @@ class _GoogleMobileAdsSampleState extends State<GoogleMobileAdsSample> {
   void dispose() {
     _bannerAd?.dispose();
     _interstitialAd?.dispose();
+
     super.dispose();
   }
 
@@ -93,25 +97,35 @@ class _GoogleMobileAdsSampleState extends State<GoogleMobileAdsSample> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: _isBannerLoaded
-            ? Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text('Banner AD'),
-                  const SizedBox(height: 12.0),
-                  SizedBox(
-                    width: _bannerAd!.size.width.toDouble(),
-                    height: _bannerAd!.size.height.toDouble(),
-                    child: AdWidget(ad: _bannerAd!),
-                  ),
-                  const SizedBox(height: 24.0),
-                  ElevatedButton(
-                    onPressed: _showInterstitialAd,
-                    child: const Text('Show Interstitial Ad'),
-                  ),
-                ],
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Banner Ad',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 12.0),
+            if (_isBannerLoaded && _bannerAd != null)
+              SizedBox(
+                width: _bannerAd!.size.width.toDouble(),
+                height: _bannerAd!.size.height.toDouble(),
+                child: AdWidget(
+                  ad: _bannerAd!,
+                ),
               )
-            : const CircularProgressIndicator(),
+            else
+              const CircularProgressIndicator(),
+            const SizedBox(height: 24.0),
+            ElevatedButton(
+              onPressed: _showInterstitial,
+              child: const Text(
+                'Show Interstitial Ad',
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
