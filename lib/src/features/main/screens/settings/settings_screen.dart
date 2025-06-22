21582a806288bcaa2af9f3869529e168cd8771f2
dariomatias-dev <matiasdario75@gmail.com>
2025-06-22 07:57:23 -0300

    feat(lib/src/features/main/screens/settings/settings_screen.dart): added access to official site and privacy policy
    - Added buttons in the settings screen to access the official app website and its privacy policy.

--- lib/src/features/main/screens/settings/settings_screen.dart
+++ lib/src/features/main/screens/settings/settings_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_guide/src/features/main/screens/settings/widgets/app_inf
 import 'package:flutter_guide/src/features/main/screens/settings/widgets/select_language/select_language_widget.dart';
 import 'package:flutter_guide/src/features/main/screens/settings/widgets/social_networks_widget/social_networks_widget.dart';
 
+import 'package:flutter_guide/src/shared/utils/open_url/open_url.dart';
 import 'package:flutter_guide/src/shared/widgets/list_tile_item_widget.dart';
 
 class SettingsScreen extends StatefulWidget {
@@ -45,8 +46,30 @@ class _SettingsScreenState extends State<SettingsScreen> {
               openInBrowser: true,
               onTap: null,
             ),
+            ListTileItemWidget(
+              title: 'Official Site',
+              icon: Icons.public,
+              onTap: () {
+                openUrl(
+                  () => context,
+                  'https://flutter-guide-web.vercel.app/',
+                );
+              },
+              openInBrowser: true,
+            ),
+            ListTileItemWidget(
+              title: 'Privacy Policy',
+              icon: Icons.article_outlined,
+              onTap: () {
+                openUrl(
+                  () => context,
+                  'https://flutter-guide-web.vercel.app/privacy-policy/',
+                );
+              },
+              openInBrowser: true,
+            ),
             // ListTileItemWidget(
-            //   title: 'Buy Me a Coffee',
+            //   title: 'Buy Me a Coffee',F
             //   icon: Icons.local_cafe_outlined,
             //   onTap: _controller.showDonateDialog,
             // ),
