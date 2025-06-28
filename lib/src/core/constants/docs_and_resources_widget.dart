08510830852b10b516903e98e6be2db797485335
dariomatias-dev <matiasdario75@gmail.com>
2025-06-28 10:47:47 -0300

    feat(lib/src/core/constants): repositioned `getDocsAndResources` links
    - Moved `getDocsAndResources` links to `AppLinks` to centralize application links.

--- lib/src/core/constants/docs_and_resources_widget.dart
+++ lib/src/core/constants/docs_and_resources_widget.dart
@@ -1,38 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+import 'package:flutter_guide/src/core/constants/links/app_links.dart';
+
 import 'package:flutter_guide/src/shared/models/doc_and_resources_model.dart';
 
-List<DocAndResourcesModel> getDocsAndResources(
-  BuildContext context,
-) {
+List<DocAndResourcesModel> getDocsAndResources(BuildContext context) {
   final appLocalizations = AppLocalizations.of(context)!;
 
   return <DocAndResourcesModel>[
     DocAndResourcesModel(
       name: appLocalizations.flutterDocs,
       icon: Icons.library_books,
-      url: 'https://docs.flutter.dev/',
+      url: AppLinks.flutterDocs,
     ),
     DocAndResourcesModel(
       name: appLocalizations.dartDocs,
       icon: Icons.code,
-      url: 'https://dart.dev/guides',
+      url: AppLinks.dartDocs,
     ),
     const DocAndResourcesModel(
       name: 'Dart Pad',
       icon: Icons.edit,
-      url: 'https://dartpad.dev/',
+      url: AppLinks.dartPad,
     ),
     DocAndResourcesModel(
       name: 'pub.dev - ${appLocalizations.packages}',
       icon: Icons.shop,
-      url: 'https://pub.dev/',
+      url: AppLinks.pubDev,
     ),
     DocAndResourcesModel(
       name: appLocalizations.samples,
       icon: Icons.collections,
-      url: 'https://flutter.github.io/samples/#',
+      url: AppLinks.flutterSamples,
     ),
   ];
 }
--- lib/src/core/constants/links/app_links.dart
+++ lib/src/core/constants/links/app_links.dart
@@ -3,4 +3,9 @@ class AppLinks {
   static const officialSite = 'https://flutter-guide-web.vercel.app/';
   static const privacyPolicy =
       'https://flutter-guide-web.vercel.app/privacy-policy/';
+  static const flutterDocs = 'https://docs.flutter.dev/';
+  static const dartDocs = 'https://dart.dev/guides';
+  static const dartPad = 'https://dartpad.dev/';
+  static const pubDev = 'https://pub.dev/';
+  static const flutterSamples = 'https://flutter.github.io/samples/#';
 }
