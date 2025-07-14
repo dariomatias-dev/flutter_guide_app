f46f8928b821cf9236f83763f3ec04503b424fa8
dariomatias-dev <matiasdario75@gmail.com>
2025-07-14 19:50:46 -0300

    refactor(lib/src/features/main/screens/settings/widgets/app_info_widget): simplified app version element
    - Removed redundant components from the app version element.

--- lib/src/features/main/screens/settings/widgets/app_info_widget/app_info_widget.dart
+++ lib/src/features/main/screens/settings/widgets/app_info_widget/app_info_widget.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_guide/l10n/app_localizations.dart';
 
-import 'package:flutter_guide/src/features/main/screens/settings/widgets/app_info_widget/app_version/app_version_widget.dart';
 import 'package:flutter_guide/src/features/main/screens/settings/widgets/app_info_widget/source_code_button_widget.dart';
+
+import 'package:flutter_guide/src/providers/user_preferences_inherited_widget.dart';
 
 class AppInfoWidget extends StatelessWidget {
   const AppInfoWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final textColor = Theme.of(context).textTheme.bodyMedium?.color;
+
     return Center(
       child: Column(
         children: <Widget>[
@@ -20,13 +24,19 @@ class AppInfoWidget extends StatelessWidget {
           Text(
             'FlutterGuide',
             style: TextStyle(
-              color:
-                  Theme.of(context).textTheme.bodyMedium?.color ?? Colors.black,
+              color: textColor ?? Colors.black,
               fontSize: 14.0,
               fontWeight: FontWeight.w700,
             ),
           ),
-          const AppVersionWidget(),
+          Text(
+            '${AppLocalizations.of(context)!.version} ${UserPreferencesInheritedWidget.of(context)!.appVersion}',
+            style: TextStyle(
+              color: textColor ?? Colors.black,
+              fontSize: 10.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
           const SizedBox(height: 20.0),
           const SourceCodeButtonWidget(),
         ],
