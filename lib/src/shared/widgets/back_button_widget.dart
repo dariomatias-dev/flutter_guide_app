311f1034e2cc131934e1bc4b589000ad3ab6167e
dariomatias-dev <matiasdario75@gmail.com>
2025-07-15 05:38:18 -0300

    refactor(lib/src/shared/widgets/back_button_widget.dart): simplified component
    - Removed `ValueListenableBuilder` widget as it was redundant.

--- lib/src/shared/widgets/back_button_widget.dart
+++ lib/src/shared/widgets/back_button_widget.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_guide/src/providers/user_preferences_inherited_widget.dart';
-
 class BackButtonWidget extends StatelessWidget {
   const BackButtonWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final themeController =
-        UserPreferencesInheritedWidget.of(context)!.themeController;
-
     return GestureDetector(
       onTap: () {
         Navigator.pop(context);
@@ -18,14 +13,9 @@ class BackButtonWidget extends StatelessWidget {
         decoration: const BoxDecoration(
           shape: BoxShape.circle,
         ),
-        child: ValueListenableBuilder(
-          valueListenable: themeController,
-          builder: (context, value, child) {
-            return Icon(
-              Icons.arrow_back_ios_new,
-              color: themeController.theme.colorScheme.tertiary,
-            );
-          },
+        child: Icon(
+          Icons.arrow_back_ios_new,
+          color: Theme.of(context).colorScheme.tertiary,
         ),
       ),
     );
