28562bbc6954b0c10638477932aca29617745af0
dariomatias-dev <matiasdario75@gmail.com>
2025-08-30 20:42:02 -0300

    feat(lib/src/shared/widgets/dialog/dialog_button_widget.dart): redesigned component
    - Updated design to align with the app's design style.

--- lib/src/shared/widgets/dialog/dialog_button_widget.dart
+++ lib/src/shared/widgets/dialog/dialog_button_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_guide/src/core/flutter_guide_colors.dart';
+import 'package:flutter_guide/src/core/theme/theme_controller.dart';
 
 class DialogButtonWidget extends StatelessWidget {
   const DialogButtonWidget({
@@ -16,23 +16,43 @@ class DialogButtonWidget extends StatelessWidget {
   final Color? textColor;
   final Color? backgroundColor;
 
+  BorderRadius get borderRadius => BorderRadius.circular(24.0);
+
   @override
   Widget build(BuildContext context) {
+    final isDark = ThemeController.instance.isDark;
+
+    final buttonColor = backgroundColor ??
+        Colors.blue.withAlpha(
+          isDark ? 22 : 20,
+        );
+
     return Expanded(
       child: SizedBox(
         height: 48.0,
-        child: ElevatedButton(
-          onPressed: onTap,
-          style: ElevatedButton.styleFrom(
-            backgroundColor: backgroundColor ?? FlutterGuideColors.white,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16.0),
+        child: InkWell(
+          onTap: onTap,
+          borderRadius: borderRadius,
+          overlayColor: WidgetStatePropertyAll(
+            Colors.blue.withAlpha(
+              isDark ? 28 : 15,
             ),
           ),
-          child: Text(
-            text,
-            style: TextStyle(
-              color: textColor ?? Colors.black,
+          hoverColor: Colors.blue.withAlpha(isDark ? 27 : 14),
+          child: Ink(
+            decoration: BoxDecoration(
+              color: buttonColor,
+              borderRadius: borderRadius,
+            ),
+            child: Center(
+              child: Text(
+                text,
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 15.0,
+                  color: textColor ?? Colors.blue,
+                ),
+              ),
             ),
           ),
         ),
