61814cc2bcc1bd4e94db27d3e0da5788a2e60cf2
dariomatias-dev <matiasdario75@gmail.com>
2025-09-05 10:47:23 -0300

    feat(lib/src/core/constants/links/app_links.dart): standardized links
    - Links standardized with removal of redundant characters.

--- lib/src/core/constants/links/app_links.dart
+++ lib/src/core/constants/links/app_links.dart
@@ -1,12 +1,12 @@
 class AppLinks {
-  static const officialSite = 'https://flutterguide.app/';
-  static const privacyPolicy = 'https://flutterguide.app/privacy-policy/';
-  static const flutterDocs = 'https://docs.flutter.dev/';
+  static const officialSite = 'https://flutterguide.app';
+  static const privacyPolicy = 'https://flutterguide.app/privacy-policy';
+  static const flutterDocs = 'https://docs.flutter.dev';
   static const dartDocs = 'https://dart.dev/guides';
-  static const dartPad = 'https://dartpad.dev/';
-  static const pubDev = 'https://pub.dev/';
-  static const flutterSamples = 'https://flutter.github.io/samples/#';
-  static const repository = 'https://github.com/dariomatias-dev/flutter_guide/';
+  static const dartPad = 'https://dartpad.dev';
+  static const pubDev = 'https://pub.dev';
+  static const flutterSamples = 'https://flutter.github.io/samples';
+  static const repository = 'https://github.com/dariomatias-dev/flutter_guide';
   static const playStoreDownload =
       'https://play.google.com/store/apps/details?id=com.dariomatias.flutter_guide';
   static const feedbackFormLinkPt =
@@ -15,8 +15,8 @@ class AppLinks {
       'https://docs.google.com/forms/d/e/1FAIpQLSeO3R0RXTseeZDXIHfmSw3P9jFQoTA50gT6_Mwf-IeQPstqpg/viewform?usp=sharing&ouid=116984750672904269893';
 
   // Social networks
-  static const myWebsite = 'https://dariomatias-dev.com/';
-  static const linkedIn = 'https://www.linkedin.com/in/dariomatias-dev/';
-  static const github = 'https://github.com/dariomatias-dev/';
-  static const instagram = 'https://www.instagram.com/dariomatias_dev/';
+  static const myWebsite = 'https://dariomatias-dev.com';
+  static const linkedIn = 'https://www.linkedin.com/in/dariomatias-dev';
+  static const github = 'https://github.com/dariomatias-dev';
+  static const instagram = 'https://www.instagram.com/dariomatias_dev';
 }
