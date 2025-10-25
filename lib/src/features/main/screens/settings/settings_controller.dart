73d2c7d61ee0fcce20db006906a3c08e6d21040b
dariomatias-dev <matiasdario75@gmail.com>
2025-10-25 17:02:45 -0300

    feat(lib/src/features/main/screens/settings): removed `DonateDialogWidget` component
    - Removed the `DonateDialogWidget` component as it was not necessary.

--- lib/src/features/main/screens/settings/settings_controller.dart
+++ lib/src/features/main/screens/settings/settings_controller.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_guide/src/features/main/screens/settings/widgets/about_dialog_widget.dart';
 import 'package:flutter_guide/src/features/main/screens/settings/widgets/docs_and_resources_dialog_widget.dart';
-import 'package:flutter_guide/src/features/main/screens/settings/widgets/donate_dialog_widget.dart';
 
 import 'package:flutter_guide/src/shared/utils/show_custom_dialog.dart';
 
@@ -18,17 +17,6 @@ class SettingsController {
       context: _context,
       builder: (overlayEntry) {
         return DocsAndResourcesDialogWidget(
-          overlayEntry: overlayEntry,
-        );
-      },
-    );
-  }
-
-  void showDonateDialog() {
-    showCustomDialog(
-      context: _context,
-      builder: (overlayEntry) {
-        return DonateDialogWidget(
           overlayEntry: overlayEntry,
         );
       },
