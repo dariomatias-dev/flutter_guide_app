63f6428508095ca14185a8c7dc64b13736316e52
dariomatias-dev <matiasdario75@gmail.com>
2026-05-21 05:23:32 -0300

    feat(lib/src/core): created route names class
    - Developed a class to centralize application route names.

---
+++ lib/src/core/routes/route_names.dart
@@ -0,0 +1,19 @@
+abstract final class RouteNames {
+  static const home = 'home';
+
+  static const elements = 'elements';
+
+  static const packages = 'packages';
+
+  static const settings = 'settings';
+
+  static const component = 'component';
+
+  static const catalog = 'catalog';
+
+  static const componentSample = 'component-sample';
+
+  static const savedComponents = 'saved-components';
+
+  static const codeTheme = 'code-theme';
+}
