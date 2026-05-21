ccf8d06051c79f628487f874cfdaee2d50cc334f
dariomatias-dev <matiasdario75@gmail.com>
2026-05-21 05:33:21 -0300

    refactor(lib/src): removed unused resources
    - Removed `FlutterGuideRouteNames`, `flutterGuideRoutes`, and `navigateTo` files.

--- lib/src/core/routes/flutter_guide_routes.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_guide/src/core/routes/flutter_guide_route_names.dart';
-
-// Main Screen
-import 'package:flutter_guide/src/features/main/main_screen.dart';
-
-final flutterGuideRoutes = {
-  // Main Screen
-  FlutterGuideRouteNames.main: (context) => const MainScreen(),
-};
