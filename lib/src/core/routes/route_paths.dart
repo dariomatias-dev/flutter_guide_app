b13517e6f5bbfc5ca397cdc92237d57b90dd5f7a
dariomatias-dev <matiasdario75@gmail.com>
2026-05-21 05:24:20 -0300

    feat(lib/src/core/routes): created route paths class
    - Developed a class to centralize application route paths.

---
+++ lib/src/core/routes/route_paths.dart
@@ -0,0 +1,19 @@
+abstract final class RoutePaths {
+  static const home = '/home';
+
+  static const elements = '/elements';
+
+  static const packages = '/packages';
+
+  static const settings = '/settings';
+
+  static const component = '/component/:type/:name';
+
+  static const catalog = '/catalog/:interfaceType';
+
+  static const componentSample = '/component-sample';
+
+  static const savedComponents = '/saved/:type';
+
+  static const codeTheme = '/code-theme';
+}
