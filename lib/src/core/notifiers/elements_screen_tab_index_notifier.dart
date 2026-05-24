9f0f7938d95aeff6232eccb70a6d7a225c737dba
dariomatias-dev <matiasdario75@gmail.com>
2026-05-24 06:44:10 -0300

    feat(lib/src/core/notifiers): created elements tab notifier
    - Developed a notifier for managing the state of the elements screen tab.

---
+++ lib/src/core/notifiers/elements_screen_tab_index_notifier.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ElementsScreenTabIndexNotifier extends Notifier<int> {
+  @override
+  int build() => 0;
+
+  void setIndex(int index) {
+    state = index;
+  }
+}
