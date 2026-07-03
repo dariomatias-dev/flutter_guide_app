9bde6dd6327c1794e218b122f879cb076d78c195
dariomatias-dev <matiasdario75@gmail.com>
2026-07-03 09:06:52 -0300

    refactor(lib/src): migrate `ComponentSampleScreen` consumers to `features/catalog`
    - Update `app_router` and `component_screen` to import the new `ComponentSampleScreen` from `features/catalog`.
    - Point all existing consumers to the new screen implementation.

--- lib/src/features/catalog/presentation/screens/component/component_screen.dart
+++ lib/src/features/catalog/presentation/screens/component/component_screen.dart
@@ -3,8 +3,8 @@ import 'package:flutter_guide/src/core/enums/component_type_enum.dart';
 import 'package:flutter_guide/src/features/catalog/presentation/providers/components_repository_provider.dart';
 import 'package:flutter_guide/src/features/catalog/presentation/screens/component/widgets/doc_popup_menu_item/doc_popup_menu_item_widget.dart';
 import 'package:flutter_guide/src/features/catalog/presentation/screens/component/widgets/favorite_popup_menu_item/favorite_popup_menu_item_widget.dart';
+import 'package:flutter_guide/src/features/catalog/presentation/screens/component_sample/component_sample_screen.dart';
 import 'package:flutter_guide/src/shared/utils/open_url/open_url.dart';
-import 'package:flutter_guide/src/shared/widgets/component_sample/component_sample_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ComponentScreen extends ConsumerWidget {
