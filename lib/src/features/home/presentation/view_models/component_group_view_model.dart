7cf421c601f96441e0248e4b445440cbe60d8ba4
dariomatias-dev <matiasdario75@gmail.com>
2026-07-03 08:31:49 -0300

    refactor(lib/src/features/home): remove `userPreferencesInheritedWidget` from `ComponentGroupViewModel`
    - Remove the `userPreferencesInheritedWidget` dependency from `ComponentGroupViewModel`.
    - Drop the field after the removal of the obsolete `CardWidget` parameters.
    - Simplify the view model by eliminating an unused dependency.

--- lib/src/features/home/presentation/view_models/component_group_view_model.dart
+++ lib/src/features/home/presentation/view_models/component_group_view_model.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-
-import 'package:flutter_guide/src/providers/user_preferences_inherited_widget.dart';
 import 'package:flutter_guide/src/providers/widgets_map_inherited_widget.dart';
 
 class ComponentGroupViewModel {
@@ -11,12 +9,8 @@ class ComponentGroupViewModel {
   }
 
   late final List<String> widgetNames;
-  late final UserPreferencesInheritedWidget userPreferencesInheritedWidget;
 
   void _init(BuildContext context) {
     widgetNames = ComponentsMapInheritedWidget.of(context)!.widgetNames;
-
-    userPreferencesInheritedWidget =
-        UserPreferencesInheritedWidget.of(context)!;
   }
 }
