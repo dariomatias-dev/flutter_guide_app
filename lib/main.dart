4b3a4532d74a83df9a757973ad5d9be9dff2b479
dariomatias-dev <matiasdario75@gmail.com>
2026-07-04 08:00:13 -0300

    refactor(lib/main.dart): remove `UserPreferencesInheritedWidget` and `ComponentsMapInheritedWidget`
    - Remove `UserPreferencesInheritedWidget` and `ComponentsMapInheritedWidget` from the `runApp` tree.
    - Wrap `FlutterGuideApp` directly with `ProviderScope`.
    - Remove the legacy `InheritedWidget`s after all of their consumers were migrated.

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_guide/src/core/di/shared_preferences_provider.dart';
+import 'package:flutter_guide/src/flutter_guide_app.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
-import 'package:flutter_guide/src/flutter_guide_app.dart';
-
-import 'package:flutter_guide/src/core/constants/samples/sample_definitions/functions.dart';
-import 'package:flutter_guide/src/core/constants/samples/sample_definitions/packages.dart';
-import 'package:flutter_guide/src/core/constants/samples/sample_definitions/widgets.dart';
-import 'package:flutter_guide/src/core/di/shared_preferences_provider.dart';
-import 'package:flutter_guide/src/core/enums/component_type_enum.dart';
-
-import 'package:flutter_guide/src/providers/favorite_notifier/favorite_notifier.dart';
-import 'package:flutter_guide/src/providers/user_preferences_inherited_widget.dart';
-import 'package:flutter_guide/src/providers/widgets_map_inherited_widget.dart';
-
-import 'package:flutter_guide/src/services/bookmarker_service/favorites_service.dart';
-
-import 'package:flutter_guide/src/shared/utils/get_infos.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -38,74 +24,12 @@ Future<void> main() async {
 
   final sharedPreferences = await SharedPreferences.getInstance();
 
-  // Notifiers
-  final favoriteWidgetNotifier = FavoriteWidgetNotifier('');
-  final favoriteFunctionNotifier = FavoriteFunctionNotifier('');
-  final favoritePackageNotifier = FavoritePackageNotifier('');
-
-  FavoriteNotifier getFavoriteNotifier(
-    ComponentType componentType,
-  ) {
-    switch (componentType) {
-      case ComponentType.widget:
-        return favoriteWidgetNotifier;
-      case ComponentType.function:
-        return favoriteFunctionNotifier;
-      default:
-        return favoritePackageNotifier;
-    }
-  }
-
-  // Services
-  final favoriteWidgetsService = FavoriteWidgetsService(
-    sharedPreferences: sharedPreferences,
-  );
-  final favoriteFunctionsService = FavoriteFunctionsService(
-    sharedPreferences: sharedPreferences,
-  );
-  final favoritePackagesService = FavoritePackagesService(
-    sharedPreferences: sharedPreferences,
-  );
-
-  FavoritesService getFavoriteService(
-    ComponentType componentType,
-  ) {
-    switch (componentType) {
-      case ComponentType.widget:
-        return favoriteWidgetsService;
-      case ComponentType.function:
-        return favoriteFunctionsService;
-      default:
-        return favoritePackagesService;
-    }
-  }
-
-  final widgetInfos = getInfos(widgets);
-  final functionsInfos = getInfos(functions);
-  final packagesInfos = getInfos(packages);
-
   runApp(
     ProviderScope(
       overrides: [
         sharedPreferencesProvider.overrideWithValue(sharedPreferences),
       ],
-      child: UserPreferencesInheritedWidget(
-        favoriteWidgetNotifier: favoriteWidgetNotifier,
-        favoritePackageNotifier: favoritePackageNotifier,
-        getFavoriteNotifier: getFavoriteNotifier,
-        favoriteWidgetsService: favoriteWidgetsService,
-        favoritePackagesService: favoritePackagesService,
-        getFavoriteService: getFavoriteService,
-        child: ComponentsMapInheritedWidget(
-          widgetsMap: widgetInfos.samples,
-          widgetNames: widgetInfos.componentNames,
-          packageNames: packagesInfos.componentNames,
-          packagesMap: packagesInfos.samples,
-          functionNames: functionsInfos.componentNames,
-          functionsMap: functionsInfos.samples,
-          child: const FlutterGuideApp(),
-        ),
-      ),
+      child: const FlutterGuideApp(),
     ),
   );
 }
